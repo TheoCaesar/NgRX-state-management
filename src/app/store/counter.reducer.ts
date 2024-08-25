bf4bfbb5c3d1c7947b923911f14daad7665312a5
TheoCaesar <theocaesar7@gmail.com>
2024-08-25 20:04:48 +0100

    custom reducer functions with actions as an additional argument

--- src/app/store/counter.reducer.ts
+++ src/app/store/counter.reducer.ts
@@ -3,12 +3,16 @@ import { increment } from './counter.actions';
 
 const initialState = 1000;
 
-export const counterReducer = createReducer(
-  initialState,
-  // on(increment, (startState)=>startState+1)
-  on(increment, (startState, args)=>startState+args.value)
-);
+// export const counterReducer = createReducer(
+//   initialState,
+//   on(increment, (startState, args)=>startState+args.value)
+// );
 
-// export function counterReducer(state = initialState){
-//     return state
-// }
+export function counterReducer(state = initialState, action: any){
+  //update state based on action dispatched
+  if (action.type === '[counter] Increment'){
+    // return state + 1;
+    return state + action.value;
+  }
+  return state
+}
